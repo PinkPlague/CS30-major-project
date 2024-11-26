394227a32705f3432db92a101c7c4b9d5ce70e1d
EVE <104692088+PinkPlague@users.noreply.github.com>
2024-11-26 14:15:36 -0600

    added new song file and finished dash mechanic

--- CS30-major-project/sketch.js
+++ CS30-major-project/sketch.js
@@ -1,10 +1,12 @@
 
 
 
-let playerSprite;
+let playerSprite, squares, circles;
 const PLAYER_SPEED = 5;
 let playerVars;
 let lastDash;
+let state = "startup"
+
 
 
 function setup() {
@@ -18,28 +20,48 @@ function setup() {
     iFrameTimer: 0,
     dashTimer: 0,
     dashCooldown: 300,
+    dashMultplier: 3.5,
     width: 30,
     height: 30,
     stretchedMax: 34,
-    stretchedMin: 28,
+    stretchedMin: 26,
     nonStretched: 30,
     colour: color(0, 254, 255),
   };
 
-  playerSprite = new Sprite(200, 200, 30, 30);
+  playerSprite = new Sprite(width/2, height/2, 30, 30);
   playerSprite.color = playerVars.colour;
   playerSprite.rotationLock = false;
   playerSprite.vel.x = 0;
   playerSprite.vel.y = 0;
   
+  // create obsticles group
+  //squares
+  squares = new Group();
+  squares.colour = color(252, 31, 109)
+  //circles
+  circles = new Group();
+  circles.colour = color(252, 31, 109)
+
+  //overlaps
+  playerSprite.overlaps(squares, )
+  playerSprite.overlaps(circles, )
 }
 
 function draw() {
-  background(220);
+  if (state === "startup") {
+    startupMenu();
+  }
 
-  movement();
-  border();
+
+  // background(0);
+
+  // movement();
+  // border();
+  
 }
+
+
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
@@ -50,20 +72,27 @@ function movement() {
 	if (kb.pressing("right")) {
   playerSprite.vel.x = playerVars.speed;
 	playerSprite.vel.y = 0;
+  playerSprite.height = playerVars.stretchedMin;
   } 
 	if (kb.pressing("left")) {
   playerSprite.vel.x = -playerVars.speed;
 	playerSprite.vel.y = 0;
+  playerSprite.height = playerVars.stretchedMin;
   } 
 	if (kb.pressing("up")) {
   playerSprite.vel.y = -playerVars.speed;
+  playerSprite.width = playerVars.stretchedMin;
   } 
 	if (kb.pressing("down")) {
   playerSprite.vel.y = playerVars.speed;
+  playerSprite.width = playerVars.stretchedMin;
   } 
 	if (!kb.pressing("right")&&!kb.pressing("left")&&!kb.pressing("down")&&!kb.pressing("up")) {
 	playerSprite.vel.x = 0;
 	playerSprite.vel.y = 0;
+
+  playerSprite.height = playerVars.nonStretched;
+  playerSprite.width = playerVars.nonStretched;
   }
   if (kb.pressing("right")&&kb.pressing("left")&&kb.pressing("down")&&kb.pressing("up")) {
   playerSprite.vel.x = 0;
@@ -79,9 +108,13 @@ function movement() {
   //DASH
   if (kb.presses("space")) {
   lastDash = millis();
-  playerVars.speed *= 2;
+  playerVars.speed *= playerVars.dashMultplier;
+  playerVars.invincible = true;
 	}
-
+  if (millis() > lastDash + playerVars.dashCooldown) {
+    playerVars.speed = PLAYER_SPEED;
+    playerVars.invincible = false;
+  } 
 }
 
 function border() {
@@ -100,4 +133,8 @@ function border() {
     if (playerSprite.y > height-15){
     playerSprite.y = height-15;
     }
+}
+
+function startupMenu() {
+  background(30, 5, 20);
 }
